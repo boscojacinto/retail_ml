70069bec57faf709d75e11d7047df081ee4b3c0c
BoscoJacinto <boscojac@gmail.com>
2025-08-31 18:48:27 +0530

    refactor: Cleaned data; removed competition price to understand sales amt w/o duplicates
    Signed-off-by: BoscoJacinto <boscojac@gmail.com>

--- retail_v2.py
+++ retail_v2.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split 
 from sklearn.metrics import mean_squared_error
@@ -8,13 +9,46 @@ from sklearn.preprocessing import StandardScaler
 # Dataset from https://statso.io/2024/08/19/price-elasticity-of-demand-case-study/
 df = pd.read_csv('Competition_Data.csv')
 
+def clean_dataset(df):
+	df = df.drop(['Index', 'Competition_Price'], axis=1)
+	df = df.drop_duplicates(subset=[
+		'Fiscal_Week_ID', 'Store_ID',
+		'Item_ID', 'Price', 'Item_Quantity',
+		'Sales_Amount_No_Discount', 'Sales_Amount']).reset_index(drop=True)
+	return df
+
+def group(df, store_id: str, item_id: str):
+	per_store_item = df.groupby(['Store_ID', 'Item_ID'])
+	for name, group in per_store_item:
+	    if name == (store_id, item_id):
+	    	print(f"Group: {name}")
+	    	print(group)
+	    	print()
+
+# def plot():
+# 	# df['Quantity_Change'] = df.groupby(['Store_ID', 'Item_ID'])['Item_Quantity']	
+# 	# plt.figure(figsize=(6, 4))
+# 	# plt.scatter(df['Index'], df['Sales_Amount'], color='blue', s=100)
+# 	# plt.title('Index vs. Sales_Amount')
+# 	# plt.xlabel('Index')
+# 	# plt.ylabel('Sales_Amount')
+# 	# plt.grid(True)
+# 	# plt.savefig('./index_vs_sales_amount.png')
+# 	# plt.close()
+
+#pd.set_option('display.max_columns', None)
+
+df = clean_dataset(df)
 print(df)
-#    Index Fiscal_Week_ID   Store_ID   Item_ID   Price  Item_Quantity	Sales_Amount_No_Discount	Sales_Amount 	Sales_Amount
-# 0      0        2019-11  store_459  item_526  134.49            435   				4716.74 		11272.59 		206.44
-# 1      1        2019-11  store_459  item_526  134.49            435   				4716.74 		11272.59 		158.01
-# 2      2        2019-11  store_459  item_526  134.49            435   				4716.74 		11272.59 		278.03
-# 3      3        2019-11  store_459  item_526  134.49            435   				4716.74 		11272.59 		222.66
-# 4      4        2019-11  store_459  item_526  134.49            435 					4716.74 		11272.59 		195.32
+group(df, 'store_162', 'item_743')
+#plot()
+
+#    Index Fiscal_Week_ID   Store_ID   Item_ID   Price  Item_Quantity	Sales_Amount_No_Discount	Sales_Amount 	Competition_Price
+# 0      0        2019-11  store_459  item_526  134.49            435   				4716.74 		11272.59 			206.44
+# 1      1        2019-11  store_459  item_526  134.49            435   				4716.74 		11272.59 			158.01
+# 2      2        2019-11  store_459  item_526  134.49            435   				4716.74 		11272.59 			278.03
+# 3      3        2019-11  store_459  item_526  134.49            435   				4716.74 		11272.59 			222.66
+# 4      4        2019-11  store_459  item_526  134.49            435 					4716.74 		11272.59 			195.32
 
 def calculate_ped():
 	# PED = % change in demand / % change in price
@@ -23,8 +57,8 @@ def calculate_ped():
 	df['Price_Change'] = df.groupby(['Store_ID', 'Item_ID'])['Price'].pct_change()
 	df['Quantity_Change'] = df.groupby(['Store_ID', 'Item_ID'])['Item_Quantity'].pct_change()
 
-	# print(f"\nPrice_Change:{df['Price_Change']}")
-	# print(f"\nQuantity_Change:{df['Quantity_Change']}")
+	print(f"\nPrice_Change:{df['Price_Change'].describe()}")
+	print(f"\nQuantity_Change:{df['Quantity_Change'].describe()}")
 
 	# PED
 	df['PED'] = df['Quantity_Change'] / df['Price_Change']
@@ -32,59 +66,51 @@ def calculate_ped():
 	df.replace([float('inf'), -float('inf')], float('nan'), inplace=True)
 	df.dropna(subset=['PED'], inplace=True)
 
-	# ped_summary = df['PED'].describe()
+	ped_summary = df['PED'].describe()
 
-	# print(ped_summary)
+	print(ped_summary)
 
 #calculate_ped()
 
-df['log_price'] = np.log1p(df['Price'])
-#df['log_ped'] = np.log1p(df['PED'])
-df['log_quantity'] = np.log1p(df['Item_Quantity'])
+def prepare_feature():
+	# Encoding
+	df['Store_ID'] = pd.factorize(df['Store_ID'])[0] + 1
+	df['Item_ID'] = pd.factorize(df['Item_ID'])[0] + 1 
+	
+	# Conver to log scale
+	df['log_price'] = np.log1p(df['Price'])
+	df['log_quantity'] = np.log1p(df['Item_Quantity'])
 
-X = df[['log_price']]
-y = df['log_quantity']
+	X = df[['log_price', 'Store_ID', 'Item_ID']]
+	y = df['log_quantity']
 
-print(df)
+	scaler = StandardScaler()
+	X_scaled = scaler.fit_transform(X)
+	# print(f"X_scaled:{X_scaled}")
+	# print(f"X mean:{X_scaled.mean()}")
+	# print(f"X std:{X_scaled.std()}")
 
-# print(f"X:{X}")
-# print(f"y:{y}")
+	return X_scaled, y
 
-scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
+# Prepare feature
+X, y = prepare_feature()
 
-X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+# Split set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+# Train
 model = LinearRegression()
 model.fit(X_train, y_train)
 
 print("Done training..")
 
+# Predict
 y_pred = model.predict(X_test)
-print(f"y_pred:{y_pred}")
-print(f"y_test:{y_test.tolist()[0:10]}")
 
-print(f"mse:{mean_squared_error(y_test, y_pred)}")
+# Evaluate
 rmse = np.sqrt(mean_squared_error(y_test, y_pred))
 print(f"RMSE (log_quantity): {rmse:.4f}")
-print(f"RMSE: {np.exp(rmse)}")
 
+# PED
 ped = model.coef_[0]
 print(f"Estimated PED (from log_price coefficient): {ped:.4f}")
-
-new_price = 134.0
-log_new_price = np.log1p(new_price)
-log_new_price_scaled = scaler.transform([[log_new_price]])
-q1 = model.predict(log_new_price_scaled)[0]  # Predicted log(quantity) at price
-delta_p = 0.01 * new_price  # 1% price increase
-log_new_price2 = np.log1p(new_price + delta_p)
-log_new_price2_scaled = scaler.transform([[log_new_price2]])
-q2 = model.predict(log_new_price2_scaled)[0]  # Predicted log(quantity) at price + delta
-numerical_ped = (q2 - q1) / (log_new_price2 - log_new_price)  # Approx. PED
-print(f"Numerical PED for price ${new_price}: {numerical_ped:.4f}")
-
-
-calculate_ped()
-provided_ped = df['PED'].mean()  # Example; use test set PED for exact validation
-print(f"Provided PED (mean): {provided_ped:.4f}")
-
